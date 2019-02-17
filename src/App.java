e3758464e9d27a70730d34c40fb64786828cc13a
mihaibalint982 <mihaibalint982@gmail.com>
2019-02-17 23:18:19 +0100

    Programmierer: Mihai Datum: 28.01.2019 Version: 0.1
    Klassen: Patient (neu)
    
    Patient v1 wurde hinzugefügt. Beinhaltet: Propetries, Konstruktor, Getter & Setter

--- src/App.java
+++ src/App.java
@@ -1,2 +1,43 @@
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class App {
+    private String Patient;
+    public static void main(String[] args) {
+
+        String username = null;
+        Console console = System.console();
+        username = console.readLine("Please enter user name : ");
+        System.out.println("You entered : " + username);
+
+
+    }
 }
+
+/*
+        // ====
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Please enter user name : ");
+        String username = null;
+        try {
+            username = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("You entered : " + username);
+
+        // ===== In Java 5, Java.util,Scanner is used for this purpose.
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter user name : ");
+        username = in.nextLine();
+        System.out.println("You entered : " + username);
+
+
+        // ====== Java 6
+        Console console = System.console();
+        username = console.readLine("Please enter user name : ");
+        System.out.println("You entered : " + username);
+ */
---
+++ src/com/spengermed/hbgm/Patient.java
@@ -0,0 +1,53 @@
+package com.spengermed.hbgm;
+
+public class Patient {
+    private String name;
+    private int age;
+    private int height;
+    private float meanHGS;
+    private float risk;
+
+    public Patient(String name, int age, int height, float meanHGS) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.meanHGS = meanHGS;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public float getMeanHGS() {
+        return meanHGS;
+    }
+
+    //TODO
+    public float getRisk() {
+        return risk;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setMeanHGS(float meanHGS) {
+        this.meanHGS = meanHGS;
+    }
+}
